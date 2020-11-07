f0174508674d75789b7a019bd33c5b00bc2ae064
orbeet <excenter8@gmail.com>
2020-11-07 18:56:40 +0200

    fix-api

--- api/contacts/contacts.router.js
+++ api/contacts/contacts.router.js
@@ -1,16 +1,16 @@
-const { Router } = require("express");
-const { ContactsController } = require("./contacts.controller");
+// const { Router } = require("express");
+// const { ContactsController } = require("./contacts.controller");
 
-const contactsRouter = Router();
+// const contactsRouter = Router();
 
-contactsRouter.get("/:contactId", ContactsController.getById);
-contactsRouter.get("/", ContactsController.listContacts);
-contactsRouter.post(
-  "/",
-  ContactsController.validateAddContact,
-  ContactsController.addContact
-);
-contactsRouter.delete("/:contactId", ContactsController.removeContact);
-contactsRouter.patch("/:contactId", ContactsController.updateContact);
+// contactsRouter.get("/:contactId", ContactsController.getById);
+// contactsRouter.get("/", ContactsController.listContacts);
+// contactsRouter.post(
+//   "/",
+//   ContactsController.validateAddContact,
+//   ContactsController.addContact
+// );
+// contactsRouter.delete("/:contactId", ContactsController.removeContact);
+// contactsRouter.patch("/:contactId", ContactsController.updateContact);
 
-module.exports = { contactsRouter };
+// module.exports = { contactsRouter };
--- api/server.js
+++ api/server.js
@@ -1,55 +1,55 @@
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const { contactsRouter } = require("./contacts/contacts.router");
+// const express = require("express");
+// const cors = require("cors");
+// const morgan = require("morgan");
+// const { contactsRouter } = require("./contacts/contacts.router");
 
-const PORT = 3000;
-const FRONTEND_URL = "http://localhost:3000";
+// const PORT = 3000;
+// const FRONTEND_URL = "http://localhost:3000";
 
-class Server {
-  constructor() {
-    this.server = null;
-  }
+// class Server {
+//   constructor() {
+//     this.server = null;
+//   }
 
-  start() {
-    this.initServer();
-    this.initMiddleware();
-    this.initRoutes();
-    this.handleErrors();
-    this.startListening();
-  }
+//   start() {
+//     this.initServer();
+//     this.initMiddleware();
+//     this.initRoutes();
+//     this.handleErrors();
+//     this.startListening();
+//   }
 
-  initServer() {
-    this.server = express();
-  }
+//   initServer() {
+//     this.server = express();
+//   }
 
-  initMiddleware() {
-    this.server.use(express.json());
-    this.server.use(cors({ origin: FRONTEND_URL }));
-    this.server.use(morgan("tiny"));
-  }
+//   initMiddleware() {
+//     this.server.use(express.json());
+//     this.server.use(cors({ origin: FRONTEND_URL }));
+//     this.server.use(morgan("tiny"));
+//   }
 
-  initRoutes() {
-    this.server.use("/api/contacts", contactsRouter);
-  }
+//   initRoutes() {
+//     this.server.use("/api/contacts", contactsRouter);
+//   }
 
-  handleErrors() {
-    this.server.use((err, req, res, next) => {
-      delete err.stack;
+//   handleErrors() {
+//     this.server.use((err, req, res, next) => {
+//       delete err.stack;
 
-      return res.status(err.status).send(`${err.name}: ${err.message}`);
-    });
-  }
+//       return res.status(err.status).send(`${err.name}: ${err.message}`);
+//     });
+//   }
 
-  initDatabase() {}
+//   initDatabase() {}
 
-  startListening() {
-    this.server.listen(PORT, () =>
-      console.log("Server started listening on port:", PORT)
-    );
-  }
-}
+//   startListening() {
+//     this.server.listen(PORT, () =>
+//       console.log("Server started listening on port:", PORT)
+//     );
+//   }
+// }
 
-module.exports = {
-  Server,
-};
+// module.exports = {
+//   Server,
+// };
